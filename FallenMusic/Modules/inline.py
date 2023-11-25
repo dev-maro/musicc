f0a7df1ab479f7288ffb24cae8d0c2498264e3dd
maro <g551552g@gamil.com>
2023-11-25 21:57:57 +0300

    first commit

---
+++ FallenMusic/Modules/inline.py
@@ -0,0 +1,72 @@
+from pyrogram.types import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InlineQueryResultPhoto,
+)
+from youtubesearchpython.__future__ import VideosSearch
+
+from FallenMusic import BOT_NAME, app
+
+
+@app.on_inline_query()
+async def inline_query_handler(_, query):
+    text = query.query.strip().lower()
+    answers = []
+    if text.strip() == "":
+        try:
+            await app.answer_inline_query(
+                query.id,
+                results=answers,
+                switch_pm_text="اكتب آي حاجة هبحث عنها في اليوتيوب ⚡",
+                cache_time=10,
+            )
+        except:
+            return
+    else:
+        a = VideosSearch(text, limit=20)
+        result = (await a.next()).get("result")
+        for x in range(15):
+            title = (result[x]["title"]).title()
+            duration = result[x]["duration"]
+            views = result[x]["viewCount"]["short"]
+            thumbnail = result[x]["thumbnails"][0]["url"].split("?")[0]
+            channellink = result[x]["channel"]["link"]
+            channel = result[x]["channel"]["name"]
+            link = result[x]["link"]
+            published = result[x]["publishedTime"]
+            description = f"{views} | {duration} Mins | {channel}  | {published}"
+            buttons = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="• ʏᴏᴜᴛᴜʙᴇ •",
+                            url=link,
+                        )
+                    ],
+                ]
+            )
+            searched_text = f"""
+≪ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ≫
+⎊ **العنوان :** [{title}]({link})
+
+⎊ **المدة :** `{duration}`دقيقه
+⎊ **المشاهدات :** `{views}`
+⎊ **نشـرت في:** {published}
+⎊ **القناة :** [{channel}]({channellink})
+
+<u>⎊ **بوآسـطه  {BOT_NAME}**</u>
+‌‌‏≪ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ≫"""
+            answers.append(
+                InlineQueryResultPhoto(
+                    photo_url=thumbnail,
+                    title=title,
+                    thumb_url=thumbnail,
+                    description=description,
+                    caption=searched_text,
+                    reply_markup=buttons,
+                )
+            )
+        try:
+            return await app.answer_inline_query(query.id, results=answers)
+        except:
+            return
---
+++ FallenMusic/Modules/song.py
@@ -0,0 +1,97 @@
+import os
+
+import requests
+import yt_dlp
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from youtube_search import YoutubeSearch
+
+from FallenMusic import BOT_MENTION, BOT_USERNAME, LOGGER, app
+
+
+@app.on_message(filters.command(["song", "vsong", "video", "music"]) | filters.command(["تحميل","فيديو","صوت"],prefixes= ["/", "!","","#"]))
+async def song(_, message: Message):
+    try:
+        await message.delete()
+    except:
+        pass
+    m = await message.reply_text("⎊ جارٍ التحميل...")
+
+    query = "".join(" " + str(i) for i in message.command[1:])
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+    try:
+        results = YoutubeSearch(query, max_results=5).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"thumb{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+        duration = results[0]["duration"]
+
+    except Exception as ex:
+        LOGGER.error(ex)
+        return await m.edit_text(
+            f"فشل إحضار المسار من ʏᴛ-ᴅʟ.\n\n**السبب :** `{ex}`"
+        )
+
+    await m.edit_text("⎊ جارٍ التحميل انتظر,\n\n⎊ بواسطه البوب")
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = f"≪ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ≫\n⎊ **العنوان :** [{title[:23]}]({link})\n⎊ **المده :** `{duration}`\n⎊ ** بواسطة :** {BOT_MENTION}\n≪ 𖧊 𝐒𝐎𝐔𝐑𝐂𝐄 𝐀𝐋𝐏𝐎𝐏 𖧊 ≫"
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(dur_arr[i]) * secmul
+            secmul *= 60
+        try:
+            visit_butt = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="يوتيوب",
+                            url=link,
+                        )
+                    ]
+                ]
+            )
+            await app.send_audio(
+                chat_id=message.from_user.id,
+                audio=audio_file,
+                caption=rep,
+                thumb=thumb_name,
+                title=title,
+                duration=dur,
+                reply_markup=visit_butt,
+            )
+            if message.chat.type != ChatType.PRIVATE:
+                await message.reply_text(
+                    "ادخل للبوت يغالي تم ارسال الاغنيه اليك."
+                )
+        except:
+            start_butt = InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="اضغط هنا",
+                            url=f"https://t.me/{BOT_USERNAME}?start",
+                        )
+                    ]
+                ]
+            )
+            return await m.edit_text(
+                text="اضغط فوق الزر أدناه وابدأ في تنزيل الأغاني",
+                reply_markup=start_butt,
+            )
+        await m.delete()
+    except:
+        return await m.edit_text("فشل تحميل الصوت على الخادم")
+
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as ex:
+        LOGGER.error(ex)
